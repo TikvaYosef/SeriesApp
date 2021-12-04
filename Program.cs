e9cbc92c1b09c2d4d3995b385fd3e322c9edd26b
TikvaYosef <tikvayosef13@gmail.com>
2021-12-04 23:50:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+
+namespace SeriesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SeriesApp();
+        }
+
+        
+        static void SeriesApp()
+        {
+            Console.WriteLine("choose number : 1.choose 2.show 3.add episod 4.show all series");
+           
+
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 2:
+
+                    Console.WriteLine("what is the name of the serie");
+                    string showEpisod = Console.ReadLine();
+
+
+                    FileStream showS = new FileStream(@"C:\series\series.txt", FileMode.Open);
+                    using (StreamReader reader = new StreamReader(showS))
+                    {
+                        Console.WriteLine(reader.ReadLine);
+                    }
+
+                    break;
+
+                case 3:
+
+                    for (int i = 0; i < 6; i++)
+                    {
+                        Console.WriteLine("add the name of the episod");
+                        string addEpisod = Console.ReadLine();
+
+                        FileStream addS = new FileStream(@"C:\series\series.txt", FileMode.Append);
+                        using (StreamWriter writer = new StreamWriter(addS))
+                        {
+                            writer.WriteLine(addEpisod);
+                        }
+                    }
+
+
+                    SeriesApp();
+                    break;
+
+
+                case 4:
+
+                    FileStream showAS = new FileStream(@"C:\series\series.txt", FileMode.Open);
+                    using (StreamReader reader = new StreamReader(showAS))
+                    {
+                        Console.WriteLine(reader.ReadToEnd);
+                    }
+
+
+                    break;
+                    
+
+            }
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Series.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriesApp
+{
+    internal class Series : IComparable
+    {
+        public string name;
+        public string ganer;
+        public int nuOfMovie;
+
+        public Series(string name, string ganer, int nuOfMovie)
+        {
+            this.name = name;
+            this.ganer = ganer;
+            this.nuOfMovie = nuOfMovie;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Series nuOfMovie_ = (Series)obj;
+            if (this.nuOfMovie < nuOfMovie_.nuOfMovie)
+            {
+                return -1;
+            }
+            if (this.nuOfMovie > nuOfMovie_.nuOfMovie)
+            {
+                return 1;
+            }
+            return 0;
+
+        }
+    }
+}
